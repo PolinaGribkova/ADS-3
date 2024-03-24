c5865d843e2cedfe95b82b87f211adfd3a21802c
PolinaGribkova <160274174+PolinaGribkova@users.noreply.github.com>
2024-03-24 15:21:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,26 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0, right = size - 1, count = 0;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int i = mid;
+            while (i++ < size && arr[i] == value) {
+                count++;
+            }
+            i = mid;
+            while (i-- >= 0 && arr[i] == value)
+                count++;
+            return count;
+        }
+        else if (arr[mid] < value) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+  return 0; 
 }
